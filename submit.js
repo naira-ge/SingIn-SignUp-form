ec3b958e381c5498020106a914eb3ea18dea78c7
Naira <gnaira1987@gmail.com>
2021-02-17 19:12:15 +0400

    new files added

--- submit.js
+++ submit.js
@@ -117,8 +117,11 @@ const showSuccess = (input) => {
 }
 
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', (e) => {
     // prevent the form from submitting
+    const reform = e.target;
+
+    // Prevent the default form submit
     e.preventDefault();
 
     // validate fields
@@ -136,10 +139,67 @@ form.addEventListener('submit', function (e) {
     if (isFormValid) {
 
     }
-});
+      
+        // Post data using the Fetch API
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+          method: 'POST',
+          body: new FormData(reform),
+        })
+          // We turn the response into text as we expect HTML
+          .then((res) => res.text())
+      
+          // Let's turn it into an HTML document
+          .then((text) => new DOMParser().parseFromString(text, "text/html"))
+      
+          // Now we have a document to work with let's replace the <form>
+          .then((doc) => {
+            // Create result message container and copy HTML from doc
+            const result = document.createElement("div");
+            result.innerHTML = doc.body.innerHTML;
+      
+            // Allow focussing this element with JavaScript
+            result.tabIndex = -1;
+      
+            // And replace the form with the response children
+            reform.parentNode.replaceChild(result, reform);
+      
+            // Move focus to the status message
+            result.focus();
+          })
+          .catch((err) => {
+            // Unlock form elements
+            Array.from(form.elements).forEach((field) => (field.disabled = false));
+      
+            // Return focus to active element
+            lastActive.focus();
+      
+            // Hide the busy state
+            statusBusy.hidden = false;
+      
+            // Show error message
+            statusFailure.hidden = false;
+          });
+      
+        // Before we disable all the fields, remember the last active field
+        const lastActive = document.activeElement;
+      
+        // Show busy state and move focus to it
+        statusBusy.hidden = false;
+        statusBusy.tabIndex = -1;
+        statusBusy.focus();
+      
+        // Disable all form elements to prevent further input
+        Array.from(reform.elements).forEach((field) => (field.disabled = true));
+      
+        // Make sure connection failure message is hidden
+        statusFailure.hidden = true;
+      
+        // Prevent the default form submit
+        e.preventDefault();
+      });
+      
 
-
-const debounce = (fn, delay = 500) => {
+function debounce(fn, delay = 500) {
     let timeoutId;
     return (...args) => {
         // cancel the previous timer
@@ -148,10 +208,10 @@ const debounce = (fn, delay = 500) => {
         }
         // setup a new timer
         timeoutId = setTimeout(() => {
-            fn.apply(null, args)
+            fn.apply(null, args);
         }, delay);
     };
-};
+}
 
 form.addEventListener('input', debounce(function (e) {
     switch (e.target.id) {
